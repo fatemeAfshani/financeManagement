e76cad0660adb6f88a74f4c25ca3b306e6205d0a
f.afshani <fatemeh.afshani78@gmail.com>
2023-06-04 21:36:59 +0330

    feat(client): add pagination to product table

--- client/src/views/products/ProductTable.js
+++ client/src/views/products/ProductTable.js
@@ -1,4 +1,6 @@
 import {
+  CPagination,
+  CPaginationItem,
   CTable,
   CTableBody,
   CTableDataCell,
@@ -6,14 +8,15 @@ import {
   CTableHeaderCell,
   CTableRow,
 } from '@coreui/react'
+import React, { useEffect, useReducer, useState } from 'react'
+import { useAuth } from 'src/components/Auth'
 import axios from 'axios'
-import React, { useEffect, useReducer } from 'react'
-import { errorMessage } from 'src/utils'
 
 const initialState = {
   error: '',
   loading: false,
   products: [],
+  total: 0,
 }
 
 const ACTIONS = {
@@ -22,12 +25,12 @@ const ACTIONS = {
   ERROR: 'error',
 }
 
-console.log('#### process.env', process.env)
 const productsReducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.CALL_API: {
       return {
         ...state,
+        error: '',
         loading: true,
       }
     }
@@ -35,7 +38,8 @@ const productsReducer = (state, action) => {
       return {
         ...state,
         loading: false,
-        products: action.data,
+        products: action.data.products,
+        total: action.data.productsCount,
       }
     }
     case ACTIONS.ERROR: {
@@ -54,61 +58,94 @@ const productsReducer = (state, action) => {
 
 export default function ProductTable() {
   const [state, dispatch] = useReducer(productsReducer, initialState)
-  const { products, loading, error } = state
+  const { products, loading, error, total } = state
+  const limit = 10
+  const pageCount = Math.ceil(total / limit)
+  const [currentPage, setCurrentPage] = useState(1)
 
+  const { token, logout } = useAuth()
   useEffect(() => {
     dispatch({ type: ACTIONS.CALL_API })
     const getProducts = async () => {
       try {
-        let response = await axios({
-          method: 'get',
-          url: `${process.env?.REACT_APP_BASE_URL}/products`,
+        const response = await axios({
+          url: `${process.env?.REACT_APP_BASE_URL}/products?offset=${currentPage - 1}`,
+          method: 'GET',
           headers: {
-            Authorization: `Bearer ${process.env?.REACT_APP_TOKEN}`,
+            Authorization: `Bearer ${token}`,
           },
         })
-        console.log('### response', response)
-        if (response.status == 200) {
-          dispatch({ type: ACTIONS.SUCCESS, data: response.data })
-          return
-        }
+
+        dispatch({ type: ACTIONS.SUCCESS, data: response.data })
       } catch (error) {
-        console.log('### error', errorMessage(error))
-        dispatch({ type: ACTIONS.ERROR, error: errorMessage(error) })
+        if (error.response && error.response.status === 401) {
+          logout()
+        }
+        const errorMessage = error.response?.data?.error?.[0] || 'something went wrong'
+        dispatch({ type: ACTIONS.ERROR, error: errorMessage })
       }
     }
 
     getProducts()
-  }, [])
+  }, [logout, token, currentPage])
   return (
     <>
-      <h2 className="my-3">Products list</h2>
-      {error ? error : ''}
-      {loading ? loading : ''}
-      <div className="bg-white">
-        <CTable hover>
-          <CTableHead>
-            <CTableRow>
-              <CTableHeaderCell scope="col">id</CTableHeaderCell>
-              <CTableHeaderCell scope="col">name</CTableHeaderCell>
-              <CTableHeaderCell scope="col">price</CTableHeaderCell>
-              <CTableHeaderCell scope="col">isDeleted</CTableHeaderCell>
-            </CTableRow>
-          </CTableHead>
-          <CTableBody>
-            {products.map((product) => {
-              return (
-                <CTableRow key={product.id}>
-                  <CTableHeaderCell scope="row">{product.id}</CTableHeaderCell>
-                  <CTableDataCell>{product.name}</CTableDataCell>
-                  <CTableDataCell>{product.price}</CTableDataCell>
-                  <CTableDataCell>{product.isDeleted ? 'true' : 'false'}</CTableDataCell>
-                </CTableRow>
-              )
-            })}
-          </CTableBody>
-        </CTable>
-      </div>
+      <h3 className="my-3">Products list</h3>
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )}
+      {loading && (
+        <div className="spinner-border text-info" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      )}
+      <CTable className=" fs-5 table  bg-white table-striped" hover>
+        <CTableHead>
+          <CTableRow>
+            <CTableHeaderCell scope="col">id</CTableHeaderCell>
+            <CTableHeaderCell scope="col">name</CTableHeaderCell>
+            <CTableHeaderCell scope="col">price</CTableHeaderCell>
+          </CTableRow>
+        </CTableHead>
+        <CTableBody>
+          {products.map((product) => {
+            return (
+              <CTableRow key={product.id}>
+                <CTableHeaderCell scope="row">{product.id}</CTableHeaderCell>
+                <CTableDataCell>{product.name}</CTableDataCell>
+                <CTableDataCell>{product.price}</CTableDataCell>
+              </CTableRow>
+            )
+          })}
+        </CTableBody>
+      </CTable>
+      <CPagination className=" " aria-label="Page navigation">
+        <CPaginationItem
+          onClick={() => setCurrentPage((prePage) => prePage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </CPaginationItem>
+        {currentPage !== 1 && (
+          <CPaginationItem onClick={() => setCurrentPage((prePage) => prePage - 1)}>
+            {currentPage - 1}
+          </CPaginationItem>
+        )}
+        <CPaginationItem active>{currentPage}</CPaginationItem>
+        {currentPage !== pageCount && (
+          <CPaginationItem onClick={() => setCurrentPage((prePage) => prePage + 1)}>
+            {currentPage + 1}
+          </CPaginationItem>
+        )}
+        <CPaginationItem
+          onClick={() => setCurrentPage((prePage) => prePage + 1)}
+          disabled={currentPage === pageCount}
+        >
+          Next
+        </CPaginationItem>
+      </CPagination>
     </>
   )
 }
